ca690e16c35fd2a31c0bfa409d84277626b48dc0
Bak Balázs <bak.balazs95@gmail.com>
2017-11-01 16:09:29 +0100

    add shared modul

---
+++ smart-campus-shared-parent/smart-campus-shared/src/main/java/hu/unideb/smartcampus/shared/enumeration/Role.java
@@ -0,0 +1,28 @@
+package hu.unideb.smartcampus.shared.enumeration;
+
+
+/**
+ * Role for user.
+ */
+public enum Role {
+  /**
+   * Admin role.
+   */
+  ADMIN,
+  /**
+   * User, student role.
+   */
+  USER,
+  /**
+   * Faculty role.
+   */
+  FACULTY,
+  /**
+   * Staff role.
+   */
+  STAFF,
+  /**
+   * Unknown.
+   */
+  UNKNOWN;
+}
